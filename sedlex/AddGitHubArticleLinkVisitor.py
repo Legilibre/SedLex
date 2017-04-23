7a8488327ba41275d4b569fc73da617140e64aea
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-04-23 23:10:57 +0200

    Add a visitor to generate the github history link.

---
+++ sedlex/AddGitHubArticleLinkVisitor.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from AbstractVisitor import AbstractVisitor
+
+class AddGitHubArticleLinkVisitor(AbstractVisitor):
+    def __init__(self, args):
+        self.repo = args.github_repository
+        self.law_id = None
+
+        super(AddGitHubArticleLinkVisitor, self).__init__()
+
+    def visit_law_reference_node(self, node, post):
+        if post:
+            return
+
+        self.law_id = node['lawId']
+
+    def visit_article_reference_node(self, node, post):
+        if post:
+            return
+
+        node['githubHistory'] = ('https://github.com/'
+            + self.repo + '/commits/master/'
+            + 'loi_' + self.law_id + '/'
+            + 'Article_' + node['id'] + '.md')
