67b8cc6616dee46b39da19b7f4eaba3dc8dbd90f
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-04-28 10:31:05 +0200

    Template the output of AddGitHubIssueVisitor using jinja2.

--- sedlex/AddGitHubIssueVisitor.py
+++ sedlex/AddGitHubIssueVisitor.py
@@ -11,35 +11,34 @@ class AddGitHubIssueVisitor(AbstractVisitor):
         self.github = Github(args.github_token)
         self.repo = self.github.get_repo(args.github_repository)
         self.issues = list(self.repo.get_issues())
-        self.current_issue = -1
+        self.current_issue_number = -1
+        self.current_issue_link = None
 
         super(AddGitHubIssueVisitor, self).__init__()
 
     def visit_edit_node(self, node, post):
         if post:
             return
-
-        if 'commitMessage' not in node:
-            node['commitMessage'] = '(#' + str(self.current_issue) + ')'
-        else:
-            node['commitMessage'] = node['commitMessage'] + '\nGitHub: #' + str(self.current_issue)
-
+        node['githubIssue'] = self.current_issue_link
+        node['commitMessage'] = template.template_string('./template/github/commit_message.j2', {'edit': node})
 
     def visit_node(self, node):
         if 'type' in node and node['type'] == 'article':
-            title = 'Article ' + str(node['order'])
-            body = node['content']
+            title = template.template_string('./template/github/issue_title.j2', { 'article': node })
+            body = template.template_string('./template/github/issue_body.j2', { 'article': node })
             found = False
             for issue in self.issues:
                 if issue.title == title:
                     found = True
-                    node['githubIssue'] = issue.html_url
-                    self.current_issue = issue.number
+                    self.current_issue_link = issue.html_url
+                    node['githubIssue'] = self.current_issue_link
+                    self.current_issue_number = issue.number
                     if issue.body != body:
                         issue.edit(title=title, body=body)
             if not found:
                 issue = self.repo.create_issue(title=title, body=body)
-                node['githubIssue'] = issue.html_url
-                self.current_issue = issue.number
+                self.current_issue_link = issue.html_url
+                node['githubIssue'] = self.current_issue_link
+                self.current_issue_number = issue.number
 
         super(AddGitHubIssueVisitor, self).visit_node(node)
