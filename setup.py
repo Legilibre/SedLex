90ec011ebec93f4c0b0e93fc831b0f782be1b13e
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2017-08-13 11:12:10 +0200

    Add the setup.py PIP install config file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SedLex',
+    version='0.1',
+    install_requires=[
+        'html5lib',
+        'beautifulsoup4',
+        'requests',
+        'jinja2',
+        'python-gitlab'
+    ]
+)
