c57002f98aaf52596cb33bf8be7747fc0eb77e09
Seb35 <seb35@seb35.fr>
2018-07-17 23:48:03 +0200

    Wrong article when multiple articles are edited
    When multiple articles are edited (e.g. « l’article 1 et l’article 2 »),
    the second and next references were accumulated. With this change the
    code/law is fixed when cited, and the article-reference nodes search their
    nearest code/law parent.

--- sedlex/AddArcheoLexFilenameVisitor.py
+++ sedlex/AddArcheoLexFilenameVisitor.py
@@ -1,44 +1,39 @@
 import os
 
 from duralex.AbstractVisitor import AbstractVisitor
+import duralex.tree as tree
 
 class AddArcheoLexFilenameVisitor(AbstractVisitor):
     def __init__(self, repository):
         self.base = repository
-        self.filename = ''
-        self.path = ''
-
         super(AddArcheoLexFilenameVisitor, self).__init__()
 
     def visit_article_reference_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(self.path, 'Article_' + node['id'] + '.md')
-        node['filename'] = self.path
+        node_law = node
+        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+            node_law = node_law['parent']
+        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
     def visit_article_definition_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(os.path.dirname(self.path), 'Article_' + node['id'] + '.md')
-        node['filename'] = self.path
+        node_law = node
+        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+            node_law = node_law['parent']
+        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
     def visit_code_reference_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(self.path, node['id'])
-        node['repository'] = self.path
+        node['repository'] = os.path.join(self.base, node['id'])
 
     def visit_law_reference_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(self.path, 'loi_' + node['id'])
-        node['repository'] = self.path
-
-    def visit_node(self, node):
-        if 'type' in node and node['type'] == 'edit':
-            self.path = self.base;
-        super(AddArcheoLexFilenameVisitor, self).visit_node(node)
+        node['repository'] = os.path.join(self.base, 'loi_' + node['id'])
