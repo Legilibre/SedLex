Legilibre/SedLex

commit c57002f98aaf52596cb33bf8be7747fc0eb77e09

Wrong article when multiple articles are edited
When multiple articles are edited (e.g. « l’article 1 et l’article 2 »),
the second and next references were accumulated. With this change the
code/law is fixed when cited, and the article-reference nodes search their
nearest code/law parent.

sedlex/AddArcheoLexFilenameVisitor.py
@@ -1,44 +1,39 @@
 import os
 
 from duralex.AbstractVisitor import AbstractVisitor
+import duralex.tree as tree
 
 class AddArcheoLexFilenameVisitor(AbstractVisitor):
     def __init__(self, repository):
         self.base = repository
-        self.filename = ''
-        self.path = ''
-
         super(AddArcheoLexFilenameVisitor, self).__init__()
 
     def visit_article_reference_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(self.path, 'Article_' + node['id'] + '.md')
-        node['filename'] = self.path
+        node_law = node
+        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+            node_law = node_law['parent']
+        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
     def visit_article_definition_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(os.path.dirname(self.path), 'Article_' + node['id'] + '.md')
-        node['filename'] = self.path
+        node_law = node
+        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+            node_law = node_law['parent']
+        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
     def visit_code_reference_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(self.path, node['id'])
-        node['repository'] = self.path
+        node['repository'] = os.path.join(self.base, node['id'])
 
     def visit_law_reference_node(self, node, post):
         if post:
             return
 
-        self.path = os.path.join(self.path, 'loi_' + node['id'])
-        node['repository'] = self.path
-
-    def visit_node(self, node):
-        if 'type' in node and node['type'] == 'edit':
-            self.path = self.base;
-        super(AddArcheoLexFilenameVisitor, self).visit_node(node)
+        node['repository'] = os.path.join(self.base, 'loi_' + node['id'])

commit 208a52fc6576310f7b9d8d720c448186b3b3fc7a

Fix bug introduced in commit c57002f in article-definition
When a new article is added but the code/law is only defined in the reference,
the new method did not more find the code/law. This patch walk to the
(first) reference inside the same verb.

Probably a more robust method should be implemented in the future, possibly
in a visitor with proper rules to find the edited code/law.

sedlex/AddArcheoLexFilenameVisitor.py
@@ -13,7 +13,7 @@ class AddArcheoLexFilenameVisitor(AbstractVisitor):
             return
 
         node_law = node
-        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+        while 'parent' in node_law and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
             node_law = node_law['parent']
         node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
@@ -22,8 +22,14 @@ class AddArcheoLexFilenameVisitor(AbstractVisitor):
             return
 
         node_law = node
-        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+        while 'parent' in node_law and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
             node_law = node_law['parent']
+        # TODO: ugly: add an article but code/law only specified in reference, a visitor should previously wrap this article-def in a code-def
+        if 'repository' not in node_law:
+            node_law = node
+            while 'parent' in node_law and node_law['type'] != tree.TYPE_EDIT:
+                node_law = node_law['parent']
+            node_law = tree.filter_nodes(node_law, lambda x: x['type'] in [tree.TYPE_CODE_REFERENCE, tree.TYPE_LAW_REFERENCE])[0]
         node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
     def visit_code_reference_node(self, node, post):
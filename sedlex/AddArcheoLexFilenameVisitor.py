208a52fc6576310f7b9d8d720c448186b3b3fc7a
Seb35 <seb35@seb35.fr>
2018-07-18 18:57:31 +0200

    Fix bug introduced in commit c57002f in article-definition
    When a new article is added but the code/law is only defined in the reference,
    the new method did not more find the code/law. This patch walk to the
    (first) reference inside the same verb.
    
    Probably a more robust method should be implemented in the future, possibly
    in a visitor with proper rules to find the edited code/law.

--- sedlex/AddArcheoLexFilenameVisitor.py
+++ sedlex/AddArcheoLexFilenameVisitor.py
@@ -13,7 +13,7 @@ class AddArcheoLexFilenameVisitor(AbstractVisitor):
             return
 
         node_law = node
-        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+        while 'parent' in node_law and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
             node_law = node_law['parent']
         node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
@@ -22,8 +22,14 @@ class AddArcheoLexFilenameVisitor(AbstractVisitor):
             return
 
         node_law = node
-        while 'parent' in node and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
+        while 'parent' in node_law and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
             node_law = node_law['parent']
+        # TODO: ugly: add an article but code/law only specified in reference, a visitor should previously wrap this article-def in a code-def
+        if 'repository' not in node_law:
+            node_law = node
+            while 'parent' in node_law and node_law['type'] != tree.TYPE_EDIT:
+                node_law = node_law['parent']
+            node_law = tree.filter_nodes(node_law, lambda x: x['type'] in [tree.TYPE_CODE_REFERENCE, tree.TYPE_LAW_REFERENCE])[0]
         node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
 
     def visit_code_reference_node(self, node, post):
