f3e1e976fc6fd08d26199283cc3dd852cb3f2955
Seb35 <seb35@seb35.fr>
2018-10-06 16:47:54 +0200

    Archéo Lex article names are with '_' instead of ' '

--- sedlex/AddArcheoLexFilenameVisitor.py
+++ sedlex/AddArcheoLexFilenameVisitor.py
@@ -15,7 +15,7 @@ class AddArcheoLexFilenameVisitor(AbstractVisitor):
         node_law = node
         while 'parent' in node_law and node_law['type'] != tree.TYPE_CODE_REFERENCE and node_law['type'] != tree.TYPE_LAW_REFERENCE:
             node_law = node_law['parent']
-        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
+        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'].replace(' ', '_') + '.md')
 
     def visit_article_definition_node(self, node, post):
         if post:
@@ -30,16 +30,16 @@ class AddArcheoLexFilenameVisitor(AbstractVisitor):
             while 'parent' in node_law and node_law['type'] != tree.TYPE_EDIT:
                 node_law = node_law['parent']
             node_law = tree.filter_nodes(node_law, lambda x: x['type'] in [tree.TYPE_CODE_REFERENCE, tree.TYPE_LAW_REFERENCE])[0]
-        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'] + '.md')
+        node['filename'] = os.path.join(node_law['repository'], 'Article_' + node['id'].replace(' ', '_') + '.md')
 
     def visit_code_reference_node(self, node, post):
         if post:
             return
 
-        node['repository'] = os.path.join(self.base, node['id'])
+        node['repository'] = os.path.join(self.base, node['id'].replace(' ', '_'))
 
     def visit_law_reference_node(self, node, post):
         if post:
             return
 
-        node['repository'] = os.path.join(self.base, 'loi_' + node['id'])
+        node['repository'] = os.path.join(self.base, 'loi_' + node['id'].replace(' ', '_'))
